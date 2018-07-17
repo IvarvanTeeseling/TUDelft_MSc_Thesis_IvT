a06f5e232e8d088a67ea16a232f9533499b00a3c
Ivar <Ivar@Ivar-PC>
2018-07-17 11:21:07 +0200

    Can now handle stress/strain cycle per laminate ply

--- 01 Functions/Stress_Strain_Cycle.m
+++ 01 Functions/Stress_Strain_Cycle.m
@@ -1,44 +1,68 @@
-function [S_xx, e_xx, R_nom, Sm_nom, Sa_nom] = Stress_Strain_Cycle(N1, M1, N2, M2, ABD, AExx, EIxx)
+function [S_xx, e_xx, R_nom, Sm_nom, Sa_nom] = Stress_Strain_Cycle(N_1, M_1, N_2, M_2, ABD, AE_xx, EI_xx)
+% TO DO: include laminate thermal stresses
+% DO DO: include the notch effect
 
 %% Membrane strain and curvature
 %   >Note: AExx and EIxx used instead of the compliance matrix to include
 %   the plane strain condition in the y-direction (if active)
 
 % Upper adherent
-e0x_1 = N1/AExx;
-kx_1  = M1/EIxx;
+e_0x_1 = N_1/AE_xx;
+kx_1  = M_1/EI_xx;
 
 % Lower adherent
-e0x_2 = N2/AExx;
-kx_2  = M2/EIxx;
+e_0x_2 = N_2/AE_xx;
+kx_2  = M_2/EI_xx;
 
 % Mid-plane z-coordinate of the metal plies adjecent to the adhesive
 % interface (bottom ply for top adherent, top ply for bottom adherent)
 z       = ABD.zply;
 z_mid   = z(1:end-1)-(z(1:end-1)-z(2:end))/2;
-z1      = z_mid(1);
-z2      = z_mid(end);
 
-%% Aluminum facesheet total strain
-e_xx.ad1 = e0x_1-z1*kx_1;
-e_xx.ad2 = e0x_2-z2*kx_2;
+% Pre-allocate memory
+e_xx.ad1    = zeros([size(N_1) 2]);
+e_xx.ad2    = zeros([size(N_1) 2]);
+S_xx.ad1    = zeros([size(N_1) 2]);
+S_xx.ad2    = zeros([size(N_1) 2]);
+R_nom.ad2   = zeros(size(N_1,1), size(N_1,2), 2);
+R_nom.ad2   = zeros(size(N_1,1), size(N_1,2), 2);
+Sm_nom.ad2  = zeros(size(N_1,1), size(N_1,2), 2);
+Sm_nom.ad2  = zeros(size(N_1,1), size(N_1,2), 2);
+Sa_nom.ad2  = zeros(size(N_1,1), size(N_1,2), 2);
+Sa_nom.ad2  = zeros(size(N_1,1), size(N_1,2), 2);
 
-%% Aluminum facesheet total stress
-% TO DO: include laminate thermal stresses
-% DO DO: include the notch effect
-S_xx.ad1 = ABD.stiff(1,1,1)*e_xx.ad1;
-S_xx.ad2 = ABD.stiff(1,1,end)*e_xx.ad2;
+cnt = 1;
 
-%% R-ratio
-R_nom.ad1 = S_xx.ad1(:,:,1)./S_xx.ad1(:,:,2);
-R_nom.ad2 = S_xx.ad2(:,:,1)./S_xx.ad2(:,:,2);
-R_nom.ad1(isnan(R_nom.ad1)) = 0;
-R_nom.ad2(isnan(R_nom.ad2)) = 0;
-
-%% Amplitude and mean stress
-Sm_nom.ad1 = (1+R_nom.ad1)./2.*S_xx.ad1(:,:,2);
-Sa_nom.ad1 = (1-R_nom.ad1)./2.*S_xx.ad1(:,:,2);
-Sm_nom.ad2 = (1+R_nom.ad2)./2.*S_xx.ad2(:,:,2);
-Sa_nom.ad2 = (1-R_nom.ad2)./2.*S_xx.ad2(:,:,2);
+% Stress/strain per ply
+for i = [1 length(z_mid)]
+    % note: e_xx.ad1 is a [NxMxPxQ] matrix where:
+    %   N = the number of cracked elements
+    %   M = the number of elements
+    %   P = 2: the min and max load
+    %   Q = 2: the two outer plies
+    
+    % Strain
+    e_xx.ad1(:,:,:,cnt) = e_0x_1-z_mid(i)*kx_1;
+    e_xx.ad2(:,:,:,cnt) = e_0x_2-z_mid(i)*kx_2;
+    
+    % Stress
+    S_xx.ad1(:,:,:,cnt) = ABD.stiff(1,1,i)*e_xx.ad1(:,:,:,cnt);
+    S_xx.ad2(:,:,:,cnt) = ABD.stiff(1,1,i)*e_xx.ad2(:,:,:,cnt);
+    
+    % R-ratio
+    R_nom.ad1(:,:,cnt)        = S_xx.ad1(:,:,1,cnt)./S_xx.ad1(:,:,2,cnt);
+    R_nom.ad2(:,:,cnt)        = S_xx.ad2(:,:,1,cnt)./S_xx.ad2(:,:,2,cnt);
+    R_nom.ad1(isnan(R_nom.ad1)) = 0;
+    R_nom.ad2(isnan(R_nom.ad2)) = 0;
+    
+    % Amplitude and mean stress
+    Sm_nom.ad1(:,:,cnt) = (1+R_nom.ad1(:,:,cnt))./2.*S_xx.ad1(:,:,2,cnt);
+    Sa_nom.ad1(:,:,cnt) = (1-R_nom.ad1(:,:,cnt))./2.*S_xx.ad1(:,:,2,cnt);
+    Sm_nom.ad2(:,:,cnt) = (1+R_nom.ad2(:,:,cnt))./2.*S_xx.ad2(:,:,2,cnt);
+    Sa_nom.ad2(:,:,cnt) = (1-R_nom.ad2(:,:,cnt))./2.*S_xx.ad2(:,:,2,cnt);
+   
+    cnt = cnt + 1;
+    
+end
 
 end
